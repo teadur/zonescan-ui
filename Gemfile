9cbe7b5f7814d4ea6eda0aa507388711f1fa4a44
Georg Kahest <georg@gj.ee>
2017-05-25 23:05:32 +0300

    Initial tests of getting data from zonescan gem completed

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'rubocop'
+  gem 'awesome_print'
 end
 
 group :development do
--- app/models/scan.rb
+++ app/models/scan.rb
@@ -1,6 +1,11 @@
 require 'zonescan'
+require 'awesome_print'
 
 class Scan < ApplicationRecord
-new Zonescan.domains_all("neti.ee")
-
+# TODO: to return to object the response must be hash
+# run = new Zonescan.run(nil,'neti.ee,google.ee,mail.ee,123feil.ee')
+# for now lets return response hashes 0 Completed 1 Failed 2 Totaltime
+ run = Zonescan.run(nil,'neti.ee,google.ee,mail.ee,123feil.ee')
+ puts "jooks: #{run.inspect}"
+ ap run
 end
